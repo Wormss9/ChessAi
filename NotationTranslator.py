e3f7a00f8338f3f9968eb95edda6259a76204a88
LordBertson <pvyboch1@gmail.com>
2019-07-18 15:20:47 +0200

    list of numbered games with the color magnus plays

--- NotationTranslator.py
+++ NotationTranslator.py
@@ -6,28 +6,39 @@ string = ''
 openBrackets = False
 openQuotes = False
 j=0
-print(contents)
+moves = []
+game = []
+games = []
+
 for i in contents:
     if i == '[':
+        if moves != []:
+            game.append(moves)
+            games.append(game)
+        moves.clear()
         openBrackets = True
     if openBrackets == False:
         if i == ' ':
-            #print(string)
             if string.endswith('.')==False:
-                print(string)
-                #print('')
+                moves.append(string)
             string = ''
         else:
             string = string + i
     else:
         #there be some code that manages which player is magnus
-        if i == '[' or i == ']':
-            if string != ' ' and string != '\n':
-                print(string)
-                string = ''
+        if i == ']':
+            if 'Carlsen, Magnus' in string:
+                game.append(j)
+                j = j + 1
+                game.append(string[:5])
+                print(j)
+            string = ''
+        elif i == '[':
+            string = ''
         else:
             string = string + i
     if i == ']':
         openBrackets = False
+
 
 
