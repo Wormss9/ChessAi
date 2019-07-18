3aabf3c38378ba3c5f7acbb91c9d82a416f0fedc
LordBertson <pvyboch1@gmail.com>
2019-07-18 12:19:42 +0200

    managing quotes, brackets and quotes, unfinished

--- NotationTranslator.py
+++ NotationTranslator.py
@@ -1,10 +1,11 @@
 import json
-import os
 
 file = open('MagnusCarlsen.pgn','r')
 contents = file.read()
 string = ''
 openBrackets = False
+openQuotes = False
+j=0
 print(contents)
 for i in contents:
     if i == '[':
@@ -13,13 +14,19 @@ for i in contents:
         if i == ' ':
             #print(string)
             if string.endswith('.')==False:
-                #print(string)
-                print('')
+                print(string)
+                #print('')
             string = ''
         else:
             string = string + i
     else:
         #there be some code that manages which player is magnus
+        if i == '[' or i == ']':
+            if string != ' ' and string != '\n':
+                print(string)
+                string = ''
+        else:
+            string = string + i
     if i == ']':
         openBrackets = False
 
