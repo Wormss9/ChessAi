4422bf6b664deeccdb643c49463b8a462352046f
LordBertson <pvyboch1@gmail.com>
2019-07-18 19:03:18 +0200

    kinda less shit but not done, it hella confusing

--- NotationTranslator.py
+++ NotationTranslator.py
@@ -1,44 +1,27 @@
 import json
 
+
 file = open('MagnusCarlsen.pgn','r')
 contents = file.read()
-string = ''
-openBrackets = False
-openQuotes = False
-j=0
+data = ''
+move = ''
 moves = []
-game = []
 games = []
-
+game = []
+with open("games.txt", "w") as output:
+    output.close()
+bracketsOpen = False
 for i in contents:
-    if i == '[':
-        if moves != []:
-            game.append(moves)
-            games.append(game)
-        moves.clear()
-        openBrackets = True
-    if openBrackets == False:
-        if i == ' ':
-            if string.endswith('.')==False:
-                moves.append(string)
-            string = ''
-        else:
-            string = string + i
+    if i == '\n':
+        if data != '':
+            if 'Carlsen, Magnus' in data:
+                print(data[1:6])
+            elif '[' not in data:
+                for j in data.split():
+                    if not j.endswith('.'):
+                        moves.append(j)
+                print(moves)
+                moves.clear()
+        data = ''
     else:
-        #there be some code that manages which player is magnus
-        if i == ']':
-            if 'Carlsen, Magnus' in string:
-                game.append(j)
-                j = j + 1
-                game.append(string[:5])
-                print(j)
-            string = ''
-        elif i == '[':
-            string = ''
-        else:
-            string = string + i
-    if i == ']':
-        openBrackets = False
-
-
-
+        data+=i
