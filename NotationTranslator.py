ad6484832ab6e45155bd8154b3086c3cd63626d3
LordBertson <pvyboch1@gmail.com>
2019-07-20 18:38:50 +0200

    refactoring stuff, documented a bit better

--- NotationTranslator.py
+++ NotationTranslator.py
@@ -1,13 +1,44 @@
 import json
 import re
+import numpy
+
+
+def fillBoard():
+    board[0,0] = board[0,7] = 1     #wr
+    board[0, 1] = board[0, 6] = 2   #wh
+    board[0,2] = board[0, 5] = 3    #wb
+    board[0,3] = 4                  #wk
+    board[0,4] = 5                  #wq
+    x = 0
+    while x < 8:
+        board[1,x] = 6              #wp
+        board[6,x] = 16             #bp
+        x+=1
+
+    board[7, 0] = board[7, 7] = 11  #br
+    board[7, 1] = board[7, 6] = 12  #wh
+    board[7, 2] = board[7, 5] = 13  #wb
+    board[7, 3] = 15                #bq
+    board[7, 4] = 14                #bk
+
+
+def processMove(move):
+    for sign in move:
+        print(sign)
+        #TO-DO: translate algebraic notation to changes in an array
+
+data = ''
+move = ''
+turn = 0;
+moves = []
+
+board = numpy.zeros((8, 8))
+fillBoard()
 
 file = open('MagnusCarlsen.pgn','r')
 contents = file.read()
-data = ''
-move = ''
-moves = []
-games = []
-game = []
+
+
 with open("games.txt", "w") as output:
     output.close()
 bracketsOpen = False
@@ -18,12 +49,14 @@ for i in contents:
                 print(data[1:6])
             elif '[' not in data:
                 for j in data.split():
-                    if not j.endswith('.') and not re.match('^.+-.+$',j):
+                    if j.endswith('.'):
+                        turn = j[:1]
+                    elif not j.endswith('.') and not re.match('^.+-.+$',j):
                         moves.append(j)
-                        #tu sa da spracovat samostatny tah
-                        # TO-DO: napisat na to funkciu
-                print(moves)
-                moves.clear()
+                if moves!= []:
+                    print(moves)
+                    moves.clear()
         data = ''
     else:
         data+=i
+
