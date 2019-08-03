d560b1cc65cf70d1ca2d118d1265d832eb68a790
LordBertson <pvyboch1@gmail.com>
2019-08-03 13:27:03 +0200

    lowered learning rate, got model with 40% accuracy

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -6,9 +6,6 @@ import matplotlib.pyplot as plt
 import csv
 import h5py
 
-param = 1
-
-early_stopping = EarlyStopping(monitor='val_loss', patience=10)
 print('Reading Input:')
 boards = np.loadtxt("./training_data/train-boards.csv",delimiter=",")
 input_shape = boards.shape[1]
@@ -19,7 +16,7 @@ output_shape = moves.shape[1]
 print('Done')
 print('All read..')
 # Stop after validation loss hasn't dropped for 5 epochs
-early_stopping = EarlyStopping(monitor='val_acc', patience=5)
+early_stopping = EarlyStopping(monitor='val_loss', patience=20, restore_best_weights=True)
 
 model = Sequential()
 # Input layer
@@ -27,12 +24,16 @@ model.add(Dense(64, input_shape=(input_shape,), activation='relu'))
 # Intermediate layers
 model.add(Dense(64, activation='relu'))
 model.add(Dense(64, kernel_initializer='uniform', activation='relu'))
-#model.add(Dense(64, kernel_initializer='uniform', activation='relu'))
-#model.add(Dense(64, kernel_initializer='uniform', activation='relu'))
+model.add(Dense(64, kernel_initializer='uniform', activation='relu'))
+model.add(Dense(64, kernel_initializer='uniform', activation='relu'))
+model.add(Dense(64, kernel_initializer='uniform', activation='relu'))
+model.add(Dense(64, kernel_initializer='uniform', activation='relu'))
+model.add(Dense(32, kernel_initializer='uniform', activation='relu'))
+model.add(Dense(16, kernel_initializer='uniform', activation='relu'))
 # Output layer
-model.add(Dense(output_shape, kernel_initializer='uniform', activation= 'relu'))
+model.add(Dense(output_shape, kernel_initializer='uniform', activation='relu'))
 # Compiling model
-model.compile(Adam(lr = 0.01), loss='mse', metrics=['acc'])
+model.compile(Adam(lr = 0.001), loss='mse', metrics=['acc'])
 # Fitting model
-history = model.fit(boards,moves, batch_size=int(input_shape), validation_split=0.1, epochs=100, verbose = 2)
-model.save('./model.h5')
+history = model.fit(boards,moves, batch_size=int(input_shape), callbacks=[early_stopping], validation_split=0.2, epochs=100, verbose = 2)
+model.save('./model3.h5')
