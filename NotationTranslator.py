64256c56b09b5c11c3a642fa240abc932ae1b608
LordBertson <pvyboch1@gmail.com>
2020-12-22 20:25:45 +0100

    All figures rewritten, untested so far

--- NotationTranslator.py
+++ NotationTranslator.py
@@ -1,4 +1,3 @@
-import json
 import csv
 import re
 import numpy
@@ -9,6 +8,7 @@ import time
 def find_direction(white):
     return (1, 0) if white else (-1, 8)
 
+
 def sign(param):
     if param > 0:
         return 1
@@ -16,6 +16,7 @@ def sign(param):
         return 0
     else:
         return -1
+
 
 def create_board():
     board = numpy.zeros((8, 8))
@@ -92,51 +93,102 @@ def pawn_move(src_x: int, src_y: int, x: int, y: int, capture: bool, white: bool
     direction, starting_end = find_direction(white)
     value = direction*6
     pawn_pos: list = numpy.argwhere(in_board == value).tolist()
+    result_list = []
     for i,j in pawn_pos.copy():
         if (src_x is not None and i != src_x) or (src_y is not None and j != src_y):
             pawn_pos.remove([i,j])
     for i,j in pawn_pos:
         if capture and x == j+direction and (y == i+1 or y == i-1) and in_board[x, y] != 0:
-            in_board = move_piece(i, j, x, y, value, direction, in_board)
+            result_list.append((i,j))
         elif j == starting_end + direction and x-i == 0 and y-j == 2*direction and in_board[x, y] == 0:
-            in_board = move_piece(i, j, x, y, value, direction, in_board)
+            result_list.append((i,j))
         elif x-i == 0 and y-j == direction and in_board[x, y] == 0:
-            in_board = move_piece(i, j, x, y, value, direction, in_board)
-        else:
-            raise Exception('Invalid move')
-    return in_board
+            result_list.append((i,j))
+    return result_list
 
 
 def queen_move(src_x: int, src_y: int, x: int, y: int, capture: bool, white: bool, in_board: numpy.ndarray):
     direction, starting_end = find_direction(white)
     value = direction * 4
-    i, j = numpy.argwhere(in_board == value).tolist()[0]
-    if sign(board[x,y]) != 0 and sign(board[x,y]) == 1*direction:
-        raise Exception('Invalid move')
-    if abs(x - i) == abs(y - j):
-        direction_x = sign(x-i)
-        direction_y = sign(y-j)
-        while abs(x-i)-1 > 0 and (y-j)-1 > 0:
-            i += direction_x
-            j += direction_y
-            if board[i, j] != 0:
-                raise Exception('Invalid move')
-        in_board = move_piece(src_x, src_y,x,y, value, direction, in_board)
-        return in_board
-    else:
-        raise Exception('Invalid move')
+    return generic_all_directions_move(src_x, src_y, x, y, in_board, value)
+
+
+def bishop_move(src_x: int, src_y: int, x: int, y: int, capture: bool, white: bool, in_board: numpy.ndarray):
+    direction, starting_end = find_direction(white)
+    value = direction * 3
+    result_list = []
+    for i, j in generic_all_directions_move(src_x, src_y, x, y, in_board, value):
+        if abs(i-x) == abs(j-y):
+            result_list.append((i,j))
+    return result_list
+
+
+def rook_move(src_x: int, src_y: int, x: int, y: int, capture: bool, white: bool, in_board: numpy.ndarray):
+    direction, starting_end = find_direction(white)
+    value = direction
+    result_list = []
+    for i, j in generic_all_directions_move(src_x, src_y, x, y, in_board, value):
+        if i-x == 0 or j-y == 0:
+            result_list.append((i,j))
+    return result_list
+
+
+def king_move(src_x: int, src_y: int, x: int, y: int, capture: bool, white: bool, in_board: numpy.ndarray):
+    direction, starting_end = find_direction(white)
+    value = 5*direction
+    result_list = []
+    for i, j in generic_all_directions_move(src_x, src_y, x, y, in_board, value):
+        if abs(i - x) == 1 or abs(j - y) == 1:
+            result_list.append((i, j))
+    return result_list
+
+
+def knight_move(src_x: int, src_y: int, x: int, y: int, capture: bool, white: bool, in_board: numpy.ndarray):
+    direction, starting_end = find_direction(white)
+    value = 2 * direction
+    piece_pos = numpy.argwhere(in_board == value).tolist()
+    result_list = []
+    for i, j in piece_pos.copy():
+        if (src_x is not None and i != src_x) or (src_y is not None and j != src_y):
+            piece_pos.remove([i, j])
+    for i,j in piece_pos:
+        if (abs(x-i) == 1 and abs(y-j) == 2) or (abs(x-i) == 2 and abs(y-j) == 1)
+            result_list.append((i,j))
+    return result_list
+
+def generic_all_directions_move(src_x: int, src_y: int, x: int, y: int, in_board: numpy.ndarray, value: int):
+    piece_pos = numpy.argwhere(in_board == value).tolist()
+    result_list = []
+    append = True
+    for i, j in piece_pos.copy():
+        if (src_x is not None and i != src_x) or (src_y is not None and j != src_y):
+            piece_pos.remove([i, j])
+    for i, j in piece_pos.copy():
+        if i - x == 0 or j - y == 0 or abs(i - x) == abs(j - y):
+            start_x = i
+            start_y = j
+            if sign(in_board[x,y]) != 0 and sign(in_board[x,y]) == sign(value):
+                append = False
+            direction_x = sign(x-i)
+            direction_y = sign(y-j)
+            while abs(x-i)-1 > 0 and (y-j)-1 > 0:
+                i += direction_x
+                j += direction_y
+                if in_board[i, j] != 0:
+                    append = False
+            if append:
+                result_list.append((start_x, start_y))
+    return result_list
 
 
 board = numpy.zeros((8, 8))
 board[1,1] = 4
-board[5,5] = -7
+board[4,4] = -7
 print(DataFrame(board))
 
 
-board = queen_move(1,1,5,5,False,True,board)
-
-print(DataFrame(board))
-
+board = queen_move(1,1,4,4,False,True,board)
+print(board)
 
 
 
