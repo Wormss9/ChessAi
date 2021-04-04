51427e7fdac0c354333878e97599aee954ba1f9d
Wormss9 <Wormss9.bd@gmail.com>
2021-04-04 03:31:20 +0200

    Creating multi file translator

--- Run.py
+++ Run.py
@@ -1,5 +1,10 @@
 import pgntofen
 import time
+import glob
+import os
+import io
+import re
+
 switcher = {
     "r": '1,',
     "n": '2,',
@@ -20,30 +25,58 @@ switcher = {
     "5": '0,0,0,0,0,',
     "6": '0,0,0,0,0,0,',
     "7": '0,0,0,0,0,0,0,',
-    "8": '0,0,0,0,0,0,0,0',
-    " ": ' ',
-    "/": '/',
+    "8": '0,0,0,0,0,0,0,0,',
+    " ": '"',
+    "/": '',
 }
-num = input('File number:')
-start = time.time()
-pgnConverter = pgntofen.PgnToFen()
-pgnConverter.resetBoard()
-file = "PGN/Test.pgn"
-finish = pgnConverter.pgnFile(file)
-f = open("FEN" + num + ".txt", "w+")
-end = time.time()
-timer=end - start
-print("To FEN: " + str(int(timer))+"sec")
-k = 0
-for x in finish['succeeded'][0][1]:
-    for let in x:
-        if(let == ' '):
-            k += 1
-            if k == 3:
-                k = 0
-                f.write("\n")
+
+def transformfen(board):
+    finboard=""
+    for lines in board.splitlines():
+        line=""
+        for letter in (lines.split(" ")[0]):
+            line+= switcher.get(letter)
+        finboard+=line
+    return finboard
+
+def transform2(board1,board2):
+    print(transformfen(board1))
+
+def transformall(moves):
+    pgnConverter = pgntofen.PgnToFen()
+    pgnConverter.resetBoard()
+    clean_moves=(re.sub("[0-9]*\.", "",moves)).split()
+    clean_moves.pop()
+    x=0
+    for move in clean_moves:
+        if x%2==0:
+            board1=pgnConverter.getFullFen()
         else:
-            f.write(switcher.get(str(let), ""))
-end = time.time()
-print("To file: "+str(int((end - start)-timer))+"sec")
-print("Total: "+str(int(end - start))+"sec")
+            transform2(board1,pgnConverter.getFullFen())
+        x=x+1
+        pgnConverter.move(move)
+
+def translate(path: str,white_won: bool):
+    if white_won:
+        victory="1-0"
+    else:
+        victory="0-1"
+    for filename in glob.glob(os.path.join(path, '*.pgn')):
+     with open(os.path.join(os.getcwd(), filename), 'r') as file:
+        print(filename)
+        line = file.readline()
+        while line:
+            line = line.replace('\n', '')
+            if '[' in line and victory in line and ']' in line and victory in line:
+                moves=""
+                while line not in ('\n', '\r\n'):
+                    line = file.readline()
+                line = file.readline()
+                while line not in ('\n', '\r\n'):
+                    moves+=line
+                    line = file.readline()
+                transformall(moves)
+            line = file.readline()
+start = time.time()
+translate("PGN/",True)
+print("Translated in %.0f" % ((time.time()-start)/60)+" minutes and %.2f" % ((time.time()-start)%60)+" seconds")
