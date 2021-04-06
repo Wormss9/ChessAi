0535f7620c607cd0d454db8296164b30ea7a76e3
Wormss9 <Wormss9.bd@gmail.com>
2021-04-07 00:06:12 +0200

    Changing to new convention

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -7,17 +7,22 @@ import csv
 import h5py
 import numpy as np
 import os
+import time
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
-
+filenumber=1
 print('Reading Input:')
-boards = np.loadtxt("./training_data/train-boards.csv",delimiter=",")
+readStart=time.time()
+boards = np.loadtxt("./data/wb"+str(filenumber)+".csv",delimiter=",")
 input_shape = boards.shape[1]
-print('Done')
+print("Done in %.0f" % ((time.time()-readStart)/60) +
+      " minutes and %.2f" % ((time.time()-readStart) % 60)+" seconds")
 print('Reading Output:')
-moves = np.loadtxt("./training_data/train-moves.csv",delimiter=",")
+readStart=time.time()
+moves = np.loadtxt("./data/wm"+str(filenumber)+".csv",delimiter=",")
 output_shape = moves.shape[1]
-print('Done')
+print("Done in %.0f" % ((time.time()-readStart)/60) +
+      " minutes and %.2f" % ((time.time()-readStart) % 60)+" seconds")
 print('All read..')
 # Stop after validation loss hasn't dropped for 5 epochs
 early_stopping = EarlyStopping(monitor='val_loss', patience=20, restore_best_weights=True)
@@ -39,5 +44,5 @@ model.add(Dense(output_shape, kernel_initializer='uniform', activation='relu'))
 # Compiling model
 model.compile(Adam(lr = 0.001), loss='mse', metrics=['acc'])
 # Fitting model
-history = model.fit(boards,moves, batch_size=int(input_shape), callbacks=[early_stopping], validation_split=0.2, epochs=100, verbose = 2)
-model.save('models/theModel.h5')
+history = model.fit(boards,moves, batch_size=int(input_shape), callbacks=[early_stopping], validation_split=0.2, epochs=5, verbose = 2)
+model.save('models/white1.h5')
