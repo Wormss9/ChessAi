0a716e1fd5ed70abface9dc963e2996257c8fa28
Wormss9 <Wormss9.bd@gmail.com>
2021-04-07 18:24:56 +0200

    Adding white and black prediction.

--- ChessMaster.py
+++ ChessMaster.py
@@ -19,14 +19,11 @@ app = FastAPI()
 
 app.add_middleware(CORSMiddleware, allow_origins=["*"])
 
-board:np.ndarray = np.array([11.0,12.0,13.0,15.0,14.0,13.0,12.0,11.0,16.0,16.0,16.0,16.0,16.0,16.0,16.0,16.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,6.0,6.0,6.0,6.0,6.0,6.0,6.0,6.0,1.0,2.0,3.0,5.0,4.0,3.0,2.0,1.0]).reshape(-1,64)
-print(whiteModel.predict(board)[0])
-
-
 @app.post("/whiteprediction/")
 async def predict(board: Board):
     board:np.ndarray = np.array(board.state).reshape(-1,64)
     #return board.shape
+    #print(str(board))
     return {"AITurn":[round(elem, 0) for elem in whiteModel.predict(board)[0].tolist()]}
 
 @app.post("/blackprediction/")
