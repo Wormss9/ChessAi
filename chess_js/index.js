d5815695d14261bbd745a47f7a325def12767ae8
Wormss9 <Wormss9.bd@gmail.com>
2021-10-24 12:59:37 +0200

    Refactoring

--- chess_js/index.js
+++ chess_js/index.js
@@ -10,23 +10,27 @@ window.processButton = () => {
         document.getElementById("error").innerHTML = "Input must containt column letter and row"
         return
     }
-    const x = parseInt(input[1] - 1)
-    const y = parseInt(input[0].charCodeAt(0) - 97)
+    const x = input[1] - 1
+    const y = input[0].charCodeAt(0) - 97
     let transformTo = 1;
     if (input.length == 3) {
-        transformTo = input[2];
+        transformTo = parseInt(input[2])
     }
-    if (x < 0 || y < 0 || x > 7 || y > 7) {
+    if (y < 0 || x > 7 || y > 7) {
         document.getElementById("error").innerHTML = "Input must point to board"
         return
     }
+    if (transformTo < 1 || transformTo > 4) {
+        document.getElementById("error").innerHTML = "Select valid piece to transform to"
+        return
+    }
     document.getElementById("error").innerHTML = ""
-    changer.change({ position: [parseInt(x), parseInt(y)], transformTo });
+    changer.change({ position: [x, y], transformTo });
 }
 window.processClick = (coordinates) => {
-    let x = parseInt(coordinates.slice(0, 1), 10);
-    let y = parseInt(coordinates.slice(1, 2), 10);
-    if (x >= 8 || isNaN(y))
+    const x = parseInt(coordinates.slice(0, 1), 10);
+    const y = parseInt(coordinates.slice(1, 2), 10);
+    if (x > 7 || isNaN(y))
         return
     changer.change({ position: [x, y] });
 }
